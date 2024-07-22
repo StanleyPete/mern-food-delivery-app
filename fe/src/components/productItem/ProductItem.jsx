6d19dca778810fbe563748f43d2e9fa3b5bc6e62
StanleyPete <petestanleyy@gmail.com>
2024-07-22 13:56:50 +0200

    Add/remove products functionality amendment - creating basketItems object in StoreContext

--- fe/src/components/productItem/ProductItem.jsx
+++ fe/src/components/productItem/ProductItem.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext} from 'react'
 import './productItem.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContext'
 
 const ProductItem = ({id, name, price, description, image}) => {
 
-    const [itemCount, setItemCount] = useState(0)
+    const {basketItems, addToBasket, removeFromBasket} = useContext(StoreContext)
+
   return (
     <div className="product-item">
         <div className="product-item-img-container">
             <img className="product-item-image" src={image} alt="Product image" />
-            {!itemCount
+            {!basketItems[id]
               ?
-              <img className="add" onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="Add product image" />
+              <img className="add" onClick={()=>addToBasket(id)} src={assets.add_icon_white} alt="Add product image" />
               :
               <div className="product-item-counter">
-                <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="Remove product image" />
-                <p>{itemCount}</p>
-                <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="Add another product image" />
+                <img onClick={()=>removeFromBasket(id)} src={assets.remove_icon_red} alt="Remove product image" />
+                <p>{basketItems[id]}</p>
+                <img onClick={()=>addToBasket(id)} src={assets.add_icon_green} alt="Add another product image" />
               </div>
 
             }
--- fe/src/context/StoreContext.jsx
+++ fe/src/context/StoreContext.jsx
@@ -1,13 +1,35 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { product_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const contextValue = {
-        product_list
+    const [basketItems, setBasketItems] = useState({});
+
+    const addToBasket = (itemId) => {
+        if(!basketItems[itemId]){
+            setBasketItems((prev)=>({...prev,[itemId]:1}));
+        }else{
+            setBasketItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+        }
     }
+
+    const removeFromBasket = (itemId) => {
+        setBasketItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+    }
+    
+    useEffect(()=>{
+        console.log(basketItems);
+    }, [basketItems])
+         
+    const contextValue = {
+        product_list,
+        basketItems,
+        setBasketItems,
+        addToBasket,
+        removeFromBasket,
+    };
 
     return (
         <StoreContext.Provider value={contextValue}>
@@ -16,4 +38,4 @@ const StoreContextProvider = (props) => {
     )
 }
 
-export default StoreContextProvider
+export default StoreContextProvider;
