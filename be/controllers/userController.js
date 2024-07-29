79d6c2e7d183e55987715ae88995f832793b9afc
StanleyPete <petestanleyy@gmail.com>
2024-07-29 09:28:07 +0200

    Log in function added

--- be/controllers/userController.js
+++ be/controllers/userController.js
@@ -3,14 +3,10 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import validator from 'validator'
 
-//User log in
-const logInUser = async(req, res) => {
-
-};
-
 const createToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET);
 }
+
 
 //User sign up
 const signUpUser = async(req, res) => {
@@ -33,18 +29,43 @@ const signUpUser = async(req, res) => {
         //Password hasing:
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-
+        
         //Creating new user account:
         const newUser = new userModel({
             name: name,
             email: email,
             password: hashedPassword
         });
-
+        
         //Saving new user in db and response:
         const user = await newUser.save();
         const token = createToken(user._id);
         res.json({success: true, token})
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"});
+    }
+};
+
+//User log in
+const logInUser = async(req, res) => {
+    const {email, password} = req.body;
+    try {
+        //Checking if user exsists in db:
+        const user = await userModel.findOne({email});
+        if (!user) {
+            return res.json({success: false, message: "User does not exist"});
+        }
+
+        //Checking if passwords matched the one in db
+        const isPasswordOk = await bcrypt.compare(password, user.password);
+        if (!isPasswordOk){
+            return res.json({success: false, message: "Incorrect password"});
+        }
+
+        const token = createToken(user._id);
+        res.json({success: true, token});
 
     } catch (error) {
         console.log(error);
