91c5407d1954825b348d010a095cc9769297b2f7
StanleyPete <petestanleyy@gmail.com>
2024-07-30 13:51:37 +0200

    addToBasket/removeFromBasket/fetchBasket integrated with frontend

--- fe/src/context/StoreContext.jsx
+++ fe/src/context/StoreContext.jsx
@@ -11,22 +11,31 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("");
     const [product_list, setProductList] = useState([]);
 
-    const addToBasket = (itemId) => {
+    const addToBasket = async (itemId) => {
         if(!basketItems[itemId]){
             setBasketItems((prev)=>({...prev,[itemId]:1}));
         }else{
             setBasketItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
         }
+
+        if (token) {
+            await axios.post(url + "/api/basket/add", {itemId}, {headers: {token}});
+        }
+
     }
 
-    const removeFromBasket = (itemId) => {
+    const removeFromBasket = async (itemId) => {
         setBasketItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+
+        if (token) {
+            await axios.post(url + "/api/basket/remove", {itemId}, {headers: {token}});
+        }
     }
     
     const getBasketTotal = () => {
         let total = 0;
         for(const item in basketItems){
-            if(basketItems[item]>0){
+            if(basketItems[item] > 0){
                 let itemInfo = product_list.find((product)=>product._id === item);
                 total += itemInfo.price*basketItems[item];
             }
@@ -37,17 +46,21 @@ const StoreContextProvider = (props) => {
     
     const fetchProductList = async () => {
         const response = await axios.get(url + "/api/products/product_list");
-        setProductList(response.data.data)
+        setProductList(response.data.data);
     }
 
+    const loadBasketData = async (token) => {
+        const response = await axios.post(url + "/api/basket/fetch", {}, {headers: {token}});
+        setBasketItems(response.data.userBasket);
+    }
 
     //localStorage data is saved in token state when page refreshed
     useEffect(() => {
-        
         async function loadProductData() {
             await fetchProductList();
             if (localStorage.getItem("token")) {
-                setToken(localStorage.getItem("token"))
+                setToken(localStorage.getItem("token"));
+                await loadBasketData(localStorage.getItem("token"));
             }
         }
         loadProductData();
